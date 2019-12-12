5248d983fb7b2595a2d6d07feed94a51f4827316
feng.lin098 <feng.lin098@gmail.com>
2019-12-12 13:30:22 -0500

    final commit

---
+++ src/main/java/com/example/demo/PrefillDate.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class PrefillDate {
+    private LocalDate prefillDate;
+
+    public LocalDate getPrefillDate() {
+        return prefillDate;
+    }
+
+    public void setPrefillDate(LocalDate prefillDate) {
+        this.prefillDate = prefillDate;
+    }
+}
